058fb0aec8b1fcd990fa8c609112b952dea3ef38
Aalsaleh1 <144814865+Aalsaleh1@users.noreply.github.com>
2024-12-31 07:40:56 +0300

    Delete src/component directory

--- src/component/Header.jsx
+++
@@ -1,6 +0,0 @@
-export default function(){
-    return     <header id="header" >
-    <img src="src/assets/investment-calculator-logo.png"  />
-    <h1>React Investment Calculator</h1>
-    </header>
-}
